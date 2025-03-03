6dcd5da1869a3f78d3005333e5be9d6c78221ebe
Quinton <quinncg.dev@gmail.com>
2025-03-02 21:54:21 -0500

    Tweaks to Template

--- Sandbox/Source/Sandbox.cs
+++ Sandbox/Source/Sandbox.cs
@@ -7,7 +7,6 @@ static class Sandbox
 	static void Main()
 	{
 		var res = Resource.Load<BinaryResource>("README.md");
-
 		Console.WriteLine(System.Text.Encoding.Default.GetString(res.Data));
 
 		Application.Run();
