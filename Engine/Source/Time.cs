b7866eaa71a02eb38f0171357710864b3d54b103
Quinton <quinncg.dev@gmail.com>
2025-04-01 17:21:06 -0400

    Imported Code from Previous Version

---
+++ Engine/Source/Time.cs
@@ -0,0 +1,20 @@
+﻿namespace Engine;
+
+public static class Time
+{
+	public static int FrameCount { get; private set; }
+	public static float Now { get; private set; }
+	public static float Delta { get; private set; }
+
+	private static float _lastTime = 0f;
+
+	internal static void Update(float time)
+	{
+		FrameCount++;
+
+		Delta = time - _lastTime;
+		_lastTime = time;
+
+		Now = time;
+	}
+}
